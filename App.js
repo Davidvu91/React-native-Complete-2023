cbeace81b00acab6b39228bfa5f6d2c2dabfbedc
Vu_Hong_Son <sonvuhong177@gmail.com>
2022-12-28 23:28:52 +0700

    first commit/ todo list

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [currentText, setCurrentText] = useState("");
+  const [listJobs, setListJobs] = useState([]);
+  const handleTextChange = (text) => {
+    setCurrentText(text);
+  };
+
+  const handlePressAdd = () => {
+    if (!currentText) return;
+    setListJobs((prev) => [...prev, currentText]);
+    setCurrentText("");
+  };
+  console.log(listJobs);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContaier}>
+        <TextInput
+          placeholder="Enter your name..."
+          style={styles.inputText}
+          onChangeText={handleTextChange}
+          value={currentText}
+        />
+        <Button title="Add job" onPress={handlePressAdd} />
+      </View>
+      <View style={styles.listJobContainer}>
+        {listJobs.map((job, index) => (
+          <Text key={index}>{job}</Text>
+        ))}
+      </View>
     </View>
   );
 }
@@ -13,8 +37,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContaier: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 25,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  inputText: {
+    width: "70%",
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    marginRight: 8,
+    padding: 8,
+  },
+  listJobContainer: {
+    flex: 4,
+    paddingTop: 25,
   },
 });
